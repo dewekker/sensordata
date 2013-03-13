2e1ec1ca5ef49d19f6e585a5f2ebdeac164af2b3
Doug Chestnut <chestnut@apache.org>
2013-03-13 02:16:52 -0400

    fixed logging to log device id and added id to CSV /data requests

--- main.py
+++ main.py
@@ -22,27 +22,26 @@ class LoggingHandler(webapp2.RequestHandler):
     def get(self):
         data = self.request.get('data',None)
         if data is not None:
-            data = data.split(',')[1:10]
-            id = self.request.get('id')
-            SensorData(device_id=id,
-                       temp=float(data[0]),
-                       humidity=float(data[1]),
-                       dewpoint=float(data[2]),
-                       pressure=float(data[3]),
-                       light=float(data[4]),
-                       wind_speed=float(data[5]),
-                       wind_direction=float(data[6]),
-                       rainfall=float(data[7]),
-                       battery_level=float(data[8])).put()
+            data = data.split(',')[0:10]
+            SensorData(device_id=str(data[0]),
+                       temp=float(data[1]),
+                       humidity=float(data[2]),
+                       dewpoint=float(data[3]),
+                       pressure=float(data[4]),
+                       light=float(data[5]),
+                       wind_speed=float(data[6]),
+                       wind_direction=float(data[7]),
+                       rainfall=float(data[8]),
+                       battery_level=float(data[9])).put()
             self.response.set_status(204)
         else:
             self.response.set_status(403)
 
 class CSVHandler(webapp2.RequestHandler):
     def get(self):
-       csv = [  sensorData.timestamp.strftime("%Y-%m-%d %H:%M:%S")+','+str(sensorData.temp) for sensorData in SensorData.query().order(-SensorData.timestamp).fetch(1000) if sensorData.temp != -1000.0 and sensorData.temp < 80.0 ]
+       csv = [  sensorData.timestamp.strftime("%Y-%m-%d %H:%M:%S")+','+str(sensorData.temp)+','+str(sensorData.device_id) for sensorData in SensorData.query(SensorData.temp != -1000.0, SensorData.temp < 80.0).order(-SensorData.timestamp).fetch(1000) ]
        self.response.headers['Content-Type'] = 'text/csv'
-       self.response.out.write( "date,temp\n"+"\n".join(csv) )
+       self.response.out.write( "date,temp,id\n"+"\n".join(csv) )
 
 app = webapp2.WSGIApplication([('/log', LoggingHandler),
                                ('/data', CSVHandler)], debug=True)
